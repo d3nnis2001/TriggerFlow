73c72eedb86bfeb02e04d965deba0a6549525885
d3nnis2001 <d3nnis.s@web.de>
2024-09-09 16:28:27 +0200

    added logo

--- frontend/TriggerFlow/src/components/NodePage/MainBar.vue
+++ frontend/TriggerFlow/src/components/NodePage/MainBar.vue
@@ -6,17 +6,16 @@
                 <div class="flex flex-col h-full">
                     <div class="flex items-center justify-between px-6 pt-4 shrink-0">
                         <span class="inline-flex items-center gap-2">
-                            <svg width="35" height="40" viewBox="0 0 35 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M25.87 18.05L23.16 17.45L25.27 20.46V29.78L32.49 23.76V13.53L29.18 14.73L25.87 18.04V18.05ZM25.27 35.49L29.18 31.58V27.67L25.27 30.98V35.49ZM20.16 17.14H20.03H20.17H20.16ZM30.1 5.19L34.89 4.81L33.08 12.33L24.1 15.67L30.08 5.2L30.1 5.19ZM5.72 14.74L2.41 13.54V23.77L9.63 29.79V20.47L11.74 17.46L9.03 18.06L5.72 14.75V14.74ZM9.63 30.98L5.72 27.67V31.58L9.63 35.49V30.98ZM4.8 5.2L10.78 15.67L1.81 12.33L0 4.81L4.79 5.19L4.8 5.2ZM24.37 21.05V34.59L22.56 37.29L20.46 39.4H14.44L12.34 37.29L10.53 34.59V21.05L12.42 18.23L17.45 26.8L22.48 18.23L24.37 21.05ZM22.85 0L22.57 0.69L17.45 13.08L12.33 0.69L12.05 0H22.85Z"
-                                    fill="var(--p-primary-color)"
-                                />
-                                <path
-                                    d="M30.69 4.21L24.37 4.81L22.57 0.69L22.86 0H26.48L30.69 4.21ZM23.75 5.67L22.66 3.08L18.05 14.24V17.14H19.7H20.03H20.16H20.2L24.1 15.7L30.11 5.19L23.75 5.67ZM4.21002 4.21L10.53 4.81L12.33 0.69L12.05 0H8.43002L4.22002 4.21H4.21002ZM21.9 17.4L20.6 18.2H14.3L13 17.4L12.4 18.2L12.42 18.23L17.45 26.8L22.48 18.23L22.5 18.2L21.9 17.4ZM4.79002 5.19L10.8 15.7L14.7 17.14H14.74H15.2H16.85V14.24L12.24 3.09L11.15 5.68L4.79002 5.2V5.19Z"
-                                    fill="var(--p-text-color)"
-                                />
-                            </svg>
-                            <span class="font-semibold text-2xl text-primary">Triggerflow</span>
+                          <svg width="35" height="35" viewBox="0 0 742 923" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd" clip-rule="evenodd" d="M368.183 276C444.398 276 506.183 214.215 506.183 138C506.183 61.7847 444.398 0 368.183 0C291.968 0 230.183 61.7847 230.183 138C230.183 214.215 291.968 276 368.183 276ZM728.768 312.823C702.029 265.977 642.37 249.664 595.515 276.388L369.394 405.359L146.859 276.925C100.041 249.905 40.1705 265.947 13.1351 312.757C-13.9004 359.566 2.13676 419.418 48.9551 446.439L270.218 574.138V824.925C270.218 879.09 314.127 923 368.292 923C422.458 923 466.367 879.09 466.367 824.925V574.923L692.345 446.034C739.2 419.31 755.507 359.669 728.768 312.823Z" fill="url(#paint0_linear_115_683)"/>
+                            <defs>
+                            <linearGradient id="paint0_linear_115_683" x1="370.814" y1="4.4212e-09" x2="371.183" y2="1928.5" gradientUnits="userSpaceOnUse">
+                            <stop stop-color="#34D399"/>
+                            <stop offset="1" stop-color="white"/>
+                            </linearGradient>
+                            </defs>
+                          </svg>
+                          <span class="font-semibold text-2xl text-primary">Triggerflow</span>
                         </span>
                         <span>
                             <Button type="button" @click="closeCallback" icon="pi pi-times" rounded outlined></Button>
